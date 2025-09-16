41f8a011e71284d3643908a89d1f916d5987137b
Atharva <atharvapandey100@gmail.com>
2025-09-16 16:50:39 +0530

    bot impovement 1

---
+++ boliseva-config.js
@@ -0,0 +1,11 @@
+window.botpress.init({
+  "botId": "41dd7105-169d-4c1c-8d0f-1b3685862d7e",
+  "clientId": "4b592635-99d6-41d8-9c23-199205981c03",
+  "userId": "user-" + Math.random().toString(36).substr(2, 9), // unique per user
+  "configuration": {
+    "botName": "BoliBot",
+    "botAvatar": "https://files.bpcontent.cloud/2025/09/10/05/20250910055607-BSGP3G91.png",
+    "composerPlaceholder": "Hello i am BoliBot, how may i assist you today",
+    "color": "#1E40AF"
+  }
+});
